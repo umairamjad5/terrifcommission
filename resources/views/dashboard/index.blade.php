6a5c45fa87fd8847fd7edf9591fb71103c9051fa
M. Umair Amjad <umairamjad52@gmail.com>
2023-06-18 12:58:24 +0500

    update dashboard

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -9,7 +9,7 @@
                 <div class="row">
                     <div class="col-12">
                         <div class="page-title-box">
-                            <h4 class="page-title">Dashboard</h4>
+                            <h4 class="page-title">Dashboard New</h4>
                         </div>
                     </div>
                 </div>     
